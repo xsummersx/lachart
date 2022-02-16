0ef9fd316855c5934fd0d4e9a21f4b1361a4687f
xsummersx <605369747@qq.com>
2022-02-16 10:58:16 +0800

    eslint

---
+++ src/components/Demo/Basic.vue
@@ -0,0 +1,62 @@
+<!--
+ * @Author: your name
+ * @Date: 2022-02-15 18:53:47
+ * @LastEditTime: 2022-02-15 19:53:21
+ * @LastEditors: Please set LastEditors
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: \lachart\src\components\HelloWorld.vue
+-->
+<template>
+  <h1>{{ msg }}</h1>
+
+  <p>
+    Recommended IDE setup:
+    <a href="https://code.visualstudio.com/" target="_blank">VSCode</a>
+    +
+    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
+  </p>
+
+  <p>See <code>README.md</code> for more information.</p>
+
+  <p>
+    <a href="https://vitejs.dev/guide/features.html" target="_blank">
+      Vite Docs
+    </a>
+    |
+    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Docs</a>
+  </p>
+
+  <button type="button" @click="count++">count is: {{ count }}</button>
+  <p>
+    Edit
+    <code>components/HelloWorld.vue</code> to test hot module replacement.
+  </p>
+</template>
+<script setup>
+import css from "../assets/css/index.css";
+import { ref } from "vue";
+defineProps({ msg: String });
+
+let count = ref(0);
+onMounted: {
+  count = 200;
+  console.log(111);
+}
+</script>
+<style scoped>
+a {
+  color: #42b983;
+}
+
+label {
+  margin: 0 0.5em;
+  font-weight: bold;
+}
+
+code {
+  background-color: #eee;
+  padding: 2px 4px;
+  border-radius: 4px;
+  color: #304455;
+}
+</style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,52 +1,22 @@
-<script setup lang="ts">
-import { ref } from 'vue'
-
-defineProps<{ msg: string }>()
-
-const count = ref(0)
-</script>
 
 <template>
-  <h1>{{ msg }}</h1>
-
-  <p>
-    Recommended IDE setup:
-    <a href="https://code.visualstudio.com/" target="_blank">VSCode</a>
-    +
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-  </p>
-
-  <p>See <code>README.md</code> for more information.</p>
-
-  <p>
-    <a href="https://vitejs.dev/guide/features.html" target="_blank">
-      Vite Docs
-    </a>
-    |
-    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Docs</a>
-  </p>
-
-  <button type="button" @click="count++">count is: {{ count }}</button>
-  <p>
-    Edit
-    <code>components/HelloWorld.vue</code> to test hot module replacement.
-  </p>
+  <div>数字{{ state.cont }}</div>
+  <div>双数{{ state.double }}</div>
+  <button type="button" @click="addBtn">ADD</button>
 </template>
-
-<style scoped>
-a {
-  color: #42b983;
+<script setup lang="ts">
+import { reactive, computed } from "vue";
+interface typeState {
+  cont: number;
+  double: number;
+  arry: Array<number>;
 }
-
-label {
-  margin: 0 0.5em;
-  font-weight: bold;
-}
-
-code {
-  background-color: #eee;
-  padding: 2px 4px;
-  border-radius: 4px;
-  color: #304455;
-}
-</style>
+const state:typeState = reactive({
+  cont: 0,
+  double: computed(() => state.cont * 2),
+  arry: [1, 2, 3, 4],
+});
+const addBtn = ():void => {
+  state.cont++;
+};
+</script>
